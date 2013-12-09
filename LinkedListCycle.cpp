2829dbddd704f2052159a957db70a118f1c4739d
zoeeer <rang.zoe1@gmail.com>
2013-12-09 11:19:30 +0800

    add info

--- LinkedListCycle.cpp
+++ LinkedListCycle.cpp
@@ -1,3 +1,11 @@
+/*** Linked List Cycle ***
+
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+*/
 #include <stdio.h>
 
 struct ListNode {
